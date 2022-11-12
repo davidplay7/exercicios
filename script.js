853975f0068646ab5a2347b2b81b4356eeaa7f84
davidk1k3k@gmail.com <davidk1k3k@gmail.com>
2022-11-12 19:54:22 -0300

    exemplos

---
+++ script.js
@@ -0,0 +1,11 @@
+const numero = 1;
+const fim = 10;
+for (let i = 1; i <= fim; i++) {
+  const num = numero*i;
+  console.log (num);
+}
+//melhorando
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i*5);
+}
