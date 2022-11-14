fbdccf50b1413980d72a56e92914fed9986bf315
davidk1k3k@gmail.com <davidk1k3k@gmail.com>
2022-11-13 23:31:57 -0300

    novoscodigos

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const numero = 1;
+/*const numero = 1;
 const fim = 10;
 for (let i = 1; i <= fim; i++) {
   const num = numero*i;
@@ -8,4 +8,14 @@ for (let i = 1; i <= fim; i++) {
 
 for (let i = 1; i <= 10; i++) {
   console.log(i*5);
+}
+*/
+
+const lista = [0,1,2,3,4,5,6,7,8];
+
+for (var i = 0; i < lista.length; i++) {
+  const numero = lista[i];
+  if(numero%2===0){
+    console.log (numero);
+    }
 }
